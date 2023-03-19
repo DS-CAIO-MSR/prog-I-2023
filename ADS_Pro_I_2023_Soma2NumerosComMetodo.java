fc59529e42ac7875c71226a3ab3e4becc8d8bef5
DS-CAIO-MSR <116529549+DS-CAIO-MSR@users.noreply.github.com>
2023-03-19 18:06:08 -0300

    Create ADS_Pro_I_2023_Soma2NumerosComMetodo.java

---
+++ ADS_Pro_I_2023_Soma2NumerosComMetodo.java
@@ -0,0 +1,25 @@
+package somaDeDoisNumeros;
+import java.util.Scanner;
+public class somaDeDoisNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		
+		int a,b,r;
+		a = teclado.nextInt();
+		b=teclado.nextInt();
+		
+		r=soma(a,b);
+		System.out.print("A soma dos valores é: " + r);
+
+	}
+	
+	public static int soma(int x, int y) {
+		int s=0;
+		s=x+y;
+		return s;
+		}
+	
+	
+}
