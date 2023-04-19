5ac32f8fe5c08cd63fdc0524668b4017c1c9f326
DS-CAIO-MSR <116529549+DS-CAIO-MSR@users.noreply.github.com>
2023-04-19 12:52:59 -0300

    Create ADS_prog_I_2023_ComparacaoComCem.java

---
+++ Operadores_Relacionais/ADS_prog_I_2023_ComparacaoComCem.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class C01{
+    public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+        int valor;
+
+        System.out.println("Informe um valor de comparação com 100: ");
+
+        valor=teclado.nextInt();
+        if(valor>100){
+            System.out.println("Sim");
+        }else if(valor==100){
+            System.out.println("Esse valor é Igual");
+        }else{
+            System.out.println("Não");
+        }
+    }
+}
