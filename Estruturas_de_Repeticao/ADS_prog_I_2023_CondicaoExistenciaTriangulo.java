c891d7a4fe95862c48bb5de4843df3e6e278cb2b
DS-CAIO-MSR <116529549+DS-CAIO-MSR@users.noreply.github.com>
2023-04-19 13:10:03 -0300

    Create ADS_prog_I_2023_CondicaoExistenciaTriangulo.java

---
+++ Estruturas_de_Repeticao/ADS_prog_I_2023_CondicaoExistenciaTriangulo.java
@@ -0,0 +1,23 @@
+public class C04{
+    public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+
+        int a,b,c;
+        System.out.println("Informe três segmentos de reta: ");
+        a=teclado.nextInt();
+        b=teclado.nextInt();
+        c=teclado.nextInt();
+
+        while(a<=0 || b<=0 || c<=0){
+            System.out.println("Inválido, " + "Digite novamente: ");
+            a=teclado.nextInt();
+            b=teclado.nextInt();
+            c=teclado.nextInt();
+        }
+        if(a+b>c && a+c>b && b+c>a){
+          System.out.print("Sim");   
+        }else{
+             System.out.print("Não");
+        }
+    }
+}
